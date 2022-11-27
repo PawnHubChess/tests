b382ee642cd2b48e6b54fafa97095efd871ea154
Florian König <32238636+koenidv@users.noreply.github.com>
2022-11-27 16:20:29 +0100

    Basic Cypress test

---
+++ cypress/e2e/play.cy.js
@@ -0,0 +1,33 @@
+beforeEach(() => {
+  cy.viewport("macbook-15");
+  cy.intercept("WebSocketConnection.ts").as("svelte");
+  cy.visit("/play");
+  cy.wait("@svelte");
+});
+
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.wait(500);
+
+    cy.get("#entirePageLocalServer").check();
+    cy.get("#input1").type("0000"); // debug id
+    cy.get("#input2").type("1234");
+    cy.get("#connectButton").click();
+
+    // Now on game page
+
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      if (
+        err.message.includes("Cannot read properties of ") ||
+        err.message.includes("srcSquare") ||
+        err.message.includes("ResizeObserver")
+      ) {
+        // Ignore these errors
+        return false;
+      }
+    });
+
+    cy.url().should("contain", "/play/game");
+    cy.get("chess-board").should("be.visible");
+  });
+});
